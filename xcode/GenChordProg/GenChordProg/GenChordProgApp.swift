b42103f132e4454f0a4b879b4596c059fbf114c6
ma-chin-365ho <st.msnr.wk@gmail.com>
2021-08-08 23:52:41 +0900

    start

---
+++ xcode/GenChordProg/GenChordProg/GenChordProgApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenChordProgApp.swift
+//  GenChordProg
+//
+//  Created by 佐藤優成 on 2021/08/08.
+//
+
+import SwiftUI
+
+@main
+struct GenChordProgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
