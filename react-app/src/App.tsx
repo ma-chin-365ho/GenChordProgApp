76a65864e696f37da6424d45e0bef1f3c838538a
ma-chin-365ho <st.msnr.wk@gmail.com>
2021-10-06 00:43:26 +0900

    react-app material-ui

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,6 @@
 import React, { useState ,useEffect} from 'react';
+import {Button, Typography, Box} from '@material-ui/core';
+import {Stack ,Grid} from '@mui/material';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,20 +12,53 @@ interface CpgIf{
 function App() {
   const [txtChordProg, setTxtChordProg] = useState({chordProg : "", tempo : ""});
 
-  useEffect(() => {
-    console.log('useEffect...');
+  const fetchCpg = () => {
     fetch('http://localhost:4000/api/v1.0/chordproggen', {method: 'GET'})
       .then(res => res.json())
       .then(data => setTxtChordProg(data));
-   },[]);
+  }
+
+  const handleOnClickUpdate = () => {
+    fetchCpg();
+  }
+
+  useEffect(() => {
+    console.log('useEffect...');
+    fetchCpg();
+  },[]);
 
   return (
     <div className="App">
-      <body>
-        {
-          txtChordProg.chordProg
-        }
-      </body>
+      <Stack spacing={3}>
+        <Typography variant="h4">
+          {" "}
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid xs={3}>
+          </Grid>
+          <Grid xs={6}>
+            <Typography variant="h3">
+              {txtChordProg.chordProg}
+            </Typography>
+          </Grid>
+          <Grid xs={3}>
+          </Grid>
+        </Grid>
+        <Typography variant="h4">
+          {"♩ = " + txtChordProg.tempo}
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid xs={4}>
+          </Grid>
+          <Grid xs={4}>
+            <Button variant="contained" color="primary" onClick={handleOnClickUpdate}>
+              UPDATE
+            </Button>
+          </Grid>
+          <Grid xs={4}>
+          </Grid>
+        </Grid>
+      </Stack>
     </div>
   );
 }
